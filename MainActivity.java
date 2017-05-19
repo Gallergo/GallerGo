f3dd6d806ca97e8732405120f6c816f6b9b854f6
jiperman <jiperman@naver.com>
2017-05-20 02:59:16 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,30 +1,111 @@
 package com.example.yu.opencvhist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+
+import org.opencv.core.Mat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    String filename1 = "C:/images/iphone1.jpeg";
-    String filename2 = "C:/images/iphone2.jpeg";
-
-    public native int CompareHistogram(String filename1, String filename2);
-
-
-    // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
         System.loadLibrary("opencv_java3");
+        System.loadLibrary("native-lib");
+    }
+
+    private Mat img1;
+    private Mat img2;
+
+    private static final String TAG = "opencv";
+    static final int PERMISSION_REQUEST_CODE = 1;
+    String[] PERMISSIONS  = {"android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    private boolean hasPermissions(String[] permissions) {
+        int ret = 0;
+        //스트링 배열에 있는 퍼미션들의 허가 상태 여부 확인
+        for (String perms : permissions){
+            ret = checkCallingOrSelfPermission(perms);
+            if (!(ret == PackageManager.PERMISSION_GRANTED)){
+                //퍼미션 허가 안된 경우
+                return false;
+            }
+
+        }
+        //모든 퍼미션이 허가된 경우
+        return true;
+    }
+
+    private void requestNecessaryPermissions(String[] permissions) {
+        //마시멜로( API 23 )이상에서 런타임 퍼미션(Runtime Permission) 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void copyFile(String filename) {
+        String baseDir = Environment.getExternalStorageDirectory().getPath();
+        String pathDir = baseDir + File.separator + filename;
+
+        AssetManager assetManager = this.getAssets();
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        try {
+            Log.d( TAG, "copyFile :: 다음 경로로 파일복사 "+ pathDir);
+            inputStream = assetManager.open(filename);
+            outputStream = new FileOutputStream(pathDir);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+            inputStream.close();
+            inputStream = null;
+            outputStream.flush();
+            outputStream.close();
+            outputStream = null;
+        } catch (Exception e) {
+            Log.d(TAG, "copyFile :: 파일 복사 중 예외 발생 "+e.toString() );
+        }
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.text);
+        double ret;
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-
+        read_image_file();
+        ret = compare(img1.getNativeObjAddr(), img2.getNativeObjAddr());
+        tv.setText("" + ret);
     }
+
+    private void read_image_file() {
+        copyFile("bear1.jpg");
+        copyFile("bear2.jpg");
+
+        img1 = new Mat();
+        img2 = new Mat();
+
+        loadImage1("bear1.jpg", img1.getNativeObjAddr());
+        loadImage2("bear2.jpg", img2.getNativeObjAddr());
+    }
+
+    public native void loadImage1(String imageFileName, long img);
+    public native void loadImage2(String imageFileName, long img);
+    public native double compare(long Image1, long Image2);
 }
