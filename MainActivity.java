adee2be2fbe0a8d674ef1cba6c8c178f7020cd45
jiperman <jiperman@naver.com>
2017-05-27 21:45:11 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -115,3 +115,4 @@ public class MainActivity extends AppCompatActivity {
     public native void loadImage2(String imageFileName, long img);
     public native int compare(long Image1, long Image2);
 }
+a
